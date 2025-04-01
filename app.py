8b86d429fbd8ffb967184ef06b9eb218f419e54d
manivenkatasai <medammani4@gmail.com>
2025-04-02 01:11:09 +0530

    Location

--- app.py
+++ app.py
@@ -975,4 +975,4 @@ class HospitalRequest(db.Model):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
